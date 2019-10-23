e7557bbabc7d08b68a4523d3fce807dc520648e1
Vivien <vivien.frebourg@outlook.com>
2019-10-23 10:42:23 +0200

    Ajout des fichiers de la production dist

---
+++ precache-manifest.fa60f33f1111a640e1bc667090aa1349.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650fa36fcdc938cd7e1e",
+    "url": "/css/chunk-vendors.4de743c0.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4a044baa6e8e20d77eabf854e8214c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0fc72587419cf3a4b59",
+    "url": "/js/about.026c7474.js"
+  },
+  {
+    "revision": "8b1707febd744a1fdf86",
+    "url": "/js/app.053cb3c7.js"
+  },
+  {
+    "revision": "650fa36fcdc938cd7e1e",
+    "url": "/js/chunk-vendors.7df9ed2b.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
